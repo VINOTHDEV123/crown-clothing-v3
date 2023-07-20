3876a2e5ff8550dd293e76d0de965575968b5a33
vinoth <vinothkumbam0@gmail.com>
2023-07-20 17:47:17 +0530

    Befor cart component styling

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,24 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
 import Button from '../button/button.component'
+
+import CartItem from '../cart-item/cart-item.component'
+
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
-    return(
+    const{cartItems} = useContext(CartContext)
+    return (
         <div className='cart-dropdown-container'>
 
-        <div className='cart-items'/>
-
-        <Button>Proceed to checkout</Button>
-
-
-
+            <div className='cart-items'>
+                {cartItems.map((item) => ( 
+                <CartItem key={item.id} cartItem={item} />
+                ))}
+            </div>
+            <Button>Proceed to checkout</Button>
         </div>
     )
 }
