6bc4ffdf7548136aef163cd7ae9429a9ea14896b
vinoth <vinothkumbam0@gmail.com>
2023-08-08 11:08:51 +0530

    Cart spell changed

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -20,7 +20,7 @@ const ProductCard = ({ product }) => {
         <span className='price'>{price}</span>
       </div>
       <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>
-        Add to card
+        Add to cart
       </Button>
     </div>
   );
