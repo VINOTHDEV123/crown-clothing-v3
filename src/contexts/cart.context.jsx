a4ca33e67b570eeab6fac583ca9025621c6241b3
vinoth <vinothkumbam0@gmail.com>
2023-11-10 17:23:47 +0530

    Cart reducer part 3 --- 3

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,5 +1,7 @@
 import { createContext,useReducer } from 'react';
 
+import {createAction } from '../utils/reducer/reducer.utils';
+
 export const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === productToAdd.id
@@ -94,15 +96,14 @@ export const CartProvider = ({ children }) => {
       0
     );
 
-    dispatch({
-      type: CART_ACTION_TYPES.SET_CART_ITEMS, 
-      payload: { 
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
       cartItems: newCartItems, 
       cartTotal: newCartTotal, 
       cartCount: newCartCount
-    },
-  });
- };
+    })
+  );
+};
 
 
     const addItemToCart = (product) => {
@@ -121,7 +122,7 @@ export const CartProvider = ({ children }) => {
     };
 
     const setIsCartOpen = (bool) => {
-      dispatch({type:CART_ACTION_TYPES.SET_IS_CART_OPEN, payload: bool});
+      dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN,bool));
     }
 
   const value = {
