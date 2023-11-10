3555c313b4062ffca1764f32811e6808f4f02219
vinoth <vinothkumbam0@gmail.com>
2023-11-10 16:47:51 +0530

    cart - icon component cartitemcount to cartcount changed

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,14 +5,14 @@ import { CartContext } from '../../contexts/cart.context';
 import {ShoppingIcon, CartIconContainer, ItemCount} from './cart-icon.styles';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen, cartItemCount } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <ItemCount>{cartItemCount}</ItemCount>
+      <ItemCount>{cartCount}</ItemCount>
     </CartIconContainer>
   );
 };
